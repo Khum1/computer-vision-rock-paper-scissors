31ca20a829a7b1ee48d0deaf9e7b64126ec6edb4
Katy Humphries <katy.humphries96@gmail.com>
2022-11-01 09:22:44 +0000

    game runes through infinitely with camera working and picking a winner

--- camera_rps.py
+++ camera_rps.py
@@ -43,13 +43,14 @@ class RPS:
         return user_choice
 
 
-    def countdown(self, t):
-        while t > 0:
-            min, sec = divmod(t,60)
-            timer = f'{min}, {sec}'
-            print(sec, end='\r')
-            cv2.waitKey(1000)
-            t -= 1
+    def countdown(self, t, start_time):
+        elapsed_time = time.time() - start_time
+        min, sec = divmod(elapsed_time,60)
+        timer = f'{min}, {sec}'
+        # print(timer, end='\r')
+        if t - elapsed_time < 0:
+            return False
+        return True
             
     def display_camera(self, data, cap):
         ret, frame = cap.read()
@@ -58,9 +59,10 @@ class RPS:
         normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
         data[0] = normalized_image
         cv2.imshow('frame', frame)
+        cv2.waitKey(1)
 
     def predict_model(self, model, data, frame):
-        prediction = model.predict(data)
+        self.prediction = model.predict(data)
         
 
 frame = []
@@ -70,29 +72,24 @@ prediction = []
 def play():
     game = RPS(frame, prediction)
     start_time = time.time()
-    while game.user_wins <3 or game.computer_wins <3:        
-        current_time = time.time()
-        elapsed_time = current_time - start_time
-        while elapsed_time < 3:
+    while game.user_wins <3 or game.computer_wins <3: 
+        start_time = time.time()       
+        while game.countdown(5, start_time) == True:
             game.display_camera(game.data, game.cap)
-            game.countdown(3)
-            game.predict_model(game.model, game.data, game.frame)
-            computer_choice = game.get_computer_choice()
-            user_choice = game.get_prediction(game.prediction)
-            print(f'The computer chose {computer_choice}')
-            print(f'You chose {user_choice}')
-            game.get_winner(computer_choice, user_choice)
+            
+        game.predict_model(game.model, game.data, game.frame)
+        computer_choice = game.get_computer_choice()
+        user_choice = game.get_prediction(game.prediction)
+        print(f'The computer chose {computer_choice}')
+        print(f'You chose {user_choice}')
+        game.get_winner(computer_choice, user_choice)
 
         if game.user_wins == 3:
             print('Congrats you won the game!')
-            break
         elif game.computer_wins == 3: 
             print("You lost the game!")
-            break
         elif cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    game.cap.release()
-    cv2.destroyAllWindows()
+            game.cap.release()
+            cv2.destroyAllWindows()
 
 play()
